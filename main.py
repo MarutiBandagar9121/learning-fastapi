535ed8702021963666b18574ced10262a31c548d
MarutiBandagar9121 <marut.bandagar9121@gmail.com>
2025-04-21 20:11:59 +0530

    created the structure for an mvc app

--- main.py
+++ main.py
@@ -1,11 +1,20 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Query
 from enum import Enum
+from pydantic import BaseModel
+from typing import Annotated
 
 class ProductStatus(str,Enum):
     PENDING="PENDING",
     IN_PROGRESS="IN_PROGRESS",
     COMPLETED="COMPLETED",
     FAILED="FAILED"
+
+class RegistrationRequest(BaseModel):
+    first_name:str
+    last_name:str
+    username:str
+    email:str
+    password:str
 
 app = FastAPI()
 
@@ -58,7 +67,34 @@ async def status_check(status: ProductStatus):
 async def getFile(filepath:str):
     return {"path":filepath}
 
-
+#path parameters
 @app.get("/query")
 async def queryParam(status:int=0,name:str=""):
     return {"status":status,"name":name}
+
+
+# with request body
+@app.post("/register")
+async def register_user(request:RegistrationRequest):
+    return {
+        "username": request.username,
+        "email": request.email,
+    }
+
+# query params and string validation
+@app.get("/queryparams")
+async def query_params(name:Annotated[str | None,Query(min_length=3,max_length=10)]=None):
+    return {"name": name}
+
+@app.get("/approveemail")
+async def approve_email(email: Annotated[str | None, Query(min_length=8, pattern=r"^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$")]=None):
+    if email is not None:
+        return {
+            "message": "Email approved",
+            "status": True,
+        }
+    else:
+        return{
+            "message": "Email not approved",
+            "status": False,
+        }
